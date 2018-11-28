205e3c4608fc78a87f129b6d788a331361464267
Sven <sven@mutzl.org>
2018-11-28 12:37:55 +0100

    Configurable port to listen

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import app from "./app";
-const PORT = 3000;
+import app from "./app"
+const PORT = (process.env.PORT) ? process.env.PORT : 80
 
 app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
+    console.log('Metaverse Explorer API server v2 listening on port ' + PORT)
 })
