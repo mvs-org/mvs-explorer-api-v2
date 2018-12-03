004901563dec1ebdb5cd8cdf9858e79bcef0ff87
Sven <sven@mutzl.org>
2018-12-03 11:09:20 +0100

    Date filters for transactions list

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -10,10 +10,18 @@ export class TransactionController {
   public getTransactions(req: Request, res: Response) {
 
     const last_known = req.query.last_known
+    const from_date = req.query.from_date
+    const to_date = req.query.to_date
+
     let output = {}
     output['rawtx'] = (req.query.raw) ? 1 : 0
-    Transaction.find({ ...( last_known && {_id: { $lt: last_known }}), orphan: 0 },output)
-      .sort({height: -1})
+    Transaction.find({
+      ...(last_known && { _id: { $lt: last_known } }),
+      ...(from_date && { _confirmed_at: { $gte: from_date } }),
+      ...(to_date && { _confirmed_at: { $lte: to_date } }),
+      orphan: 0
+    }, output)
+      .sort({ height: -1 })
       .limit(20)
       .then((result) => {
         res.json(new ResponseSuccess(result));
