a0bdb2ef281c10bca6caaa9bc5482b624d5172fc
canguruhh <sven@mutzl.org>
2019-02-03 11:12:31 +0000

    Configure redis

--- src/routes/certificate.routes.ts
+++ src/routes/certificate.routes.ts
@@ -1,23 +1,23 @@
 import { CertificateController } from '../controllers/certificate.controller'
 
 import * as options from 'apicache'
-import {createClient} from 'redis'
-import * as redis_config from '../config/redis.js'
+import { createClient } from 'redis'
+import { config as redis_config, enabled as redis_enabled } from '../config/redis.js'
 import { Application } from 'express';
 const cache = options({
-        redisClient: (redis_config.enabled) ? createClient(redis_config.config) : undefined,
-        statusCodes: {include: [200]}
-    })
-    .middleware
+  redisClient: (redis_enabled) ? createClient(redis_config) : undefined,
+  statusCodes: { include: [200] }
+})
+  .middleware
 //Define cache rules to only cache if result was successfull
 const hourCacheSuccess = cache('60 minutes'),
-     longCacheSuccess = cache('5 minutes'),
-     mediumCacheSuccess = cache('1 minutes'),
-     shortCacheSuccess = cache('20 seconds')
+  longCacheSuccess = cache('5 minutes'),
+  mediumCacheSuccess = cache('1 minutes'),
+  shortCacheSuccess = cache('20 seconds')
 
 export class CertificateRoutes {
 
-  public certificateController : CertificateController =  new CertificateController()
+  public certificateController: CertificateController = new CertificateController()
 
   public routes(app: Application): void {
     app.route('/certs').get(mediumCacheSuccess, this.certificateController.getCertificates)
