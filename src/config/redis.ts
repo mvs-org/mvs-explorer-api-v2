39afc00b35e6bea6613ad0c8cad07c0027298a56
canguruhh <sven@mutzl.org>
2019-02-03 10:44:34 +0000

    Start to configure caching

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,9 +1,7 @@
-module.exports = {
-    enabled: true,
-    config: {
+export const enabled = true
+export const config = {
         "port": (process.env.REDIS_PORT) ? process.env.REDIS_PORT : "6379",
         "host": (process.env.REDIS_HOST) ? process.env.REDIS_HOST : "127.0.0.1",
         socket_keepalive: true,
         password: undefined
-    }
-};
+}
