60a0bda31a8d72be3dd611d7562636c3360041cb
canguruhh <sven@mutzl.org>
2019-02-20 08:36:09 +0100

    List special MST

--- src/controllers/mst.controller.ts
+++ src/controllers/mst.controller.ts
@@ -9,11 +9,28 @@ export const MST_BLACKLIST = ['ETP']
 export const MST_SPECIAL = ['PARCELX.GPX', 'RIGHTBTC.RT', 'MVS.ZGC', 'MVS.ZDC']
 
 export class MSTController {
-  public getMSTs(req: Request, res: Response) {
-    const last_symbol = req.query.last_symbol
+  public getSpecialMSTs(req: Request, res: Response) {
     Asset.find({
         symbol: {
-            $gt: last_symbol,
+            $in: MST_SPECIAL,
+        },
+    })
+      .sort({
+        symbol: 1,
+      })
+      .then((result) => {
+        res.json(new ResponseSuccess(result))
+      }).catch((err) => {
+        console.error(err)
+        res.status(400).json(new ResponseError('ERR_LIST_SPECIAL_MST'))
+      })
+  }
+
+  public getMSTs(req: Request, res: Response) {
+    const lastSymbol = req.query.last_symbol || ''
+    Asset.find({
+        symbol: {
+            $gt: lastSymbol,
             $nin: MST_BLACKLIST.concat(MST_SPECIAL),
         },
     })
--- src/routes/mst.routes.ts
+++ src/routes/mst.routes.ts
@@ -1,13 +1,14 @@
 import { Application } from 'express'
 import { MSTController } from '../controllers/mst.controller'
-import { mediumCacheSuccess } from '../helpers/cache.helper'
+import { longCacheSuccess, mediumCacheSuccess } from '../helpers/cache.helper'
 
 export class MSTRoutes {
 
   public mstController: MSTController =  new MSTController()
 
   public routes(app: Application): void {
-    app.route('/mst').get(mediumCacheSuccess, this.mstController.getMSTs)
+    app.route('/msts').get(mediumCacheSuccess, this.mstController.getMSTs)
+    app.route('/msts/special').get(longCacheSuccess, this.mstController.getMSTs)
   }
 
 }
