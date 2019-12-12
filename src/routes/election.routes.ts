8398e885890249b49b82f65b252e6a8bcab3bede
Laurent Salou <laurent.salou@hotmail.fr>
2019-12-12 19:20:20 +0800

    Remove election/candidates

--- src/routes/election.routes.ts
+++ src/routes/election.routes.ts
@@ -6,7 +6,6 @@ export class ElectionRoutes {
   public ElectionController: ElectionController = new ElectionController()
 
   public routes(app: Application): void {
-    app.route('/election/candidates').get(this.ElectionController.getCandidates)
     app.route('/election/candidates/earlybird').get(this.ElectionController.getCandidatesEarlyBird)
     app.route('/election/votes').get(this.ElectionController.getVotes)
     app.route('/election/result').get(this.ElectionController.getResult)
