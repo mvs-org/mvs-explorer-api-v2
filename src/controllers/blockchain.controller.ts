08dc8e5de249be5b1889cc3759cc4675705358da
canguruhh <sven@mutzl.org>
2020-02-28 10:48:16 +0100

    Make fee estimation definable by env

--- src/controllers/blockchain.controller.ts
+++ src/controllers/blockchain.controller.ts
@@ -6,11 +6,11 @@ export class BlockchainController {
   public async getDefaultFee(req: Request, res: Response) {
       res.setHeader('Cache-Control', 'public, max-age=600, s-maxage=600')
       return res.json(new ResponseSuccess({
-        avatar: 100000000,
-        default: 100000,
-        minimum: 10000,
-        mitIssue: 100000,
-        mstIssue: 1000000000,
+        avatar: process.env.FEE_AVATAR ? parseInt(process.env.FEE_AVATAR, 10) : 100000000,
+        default: process.env.FEE_DEFAULT ? parseInt(process.env.FEE_DEFAULT, 10) : 100000,
+        minimum: process.env.FEE_MIN ? parseInt(process.env.FEE_MIN, 10) : 10000,
+        mitIssue: process.env.FEE_MIT_ISSUE ? parseInt(process.env.FEE_MIT_ISSUE, 10) : 100000,
+        mstIssue: process.env.FEE_MST_ISSUE ? parseInt(process.env.FEE_MST_ISSUE, 10) : 1000000000,
       }))
   }
 
